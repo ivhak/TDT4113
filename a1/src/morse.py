d9fcf3a7ece49d5bbb3bcc25376a773934c31d6b
ivhak <hakonseniver@yahoo.no>
2019-08-23 11:54:23 +0200

    Python part works properly

--- a1/src/morse.py
+++ a1/src/morse.py
@@ -16,49 +16,52 @@ _dot = 1
 _dash = 2
 _mpause = 3
 _lpause = 4
-
-_morse_codes = {
-        '12':     'a',
-        '2111':   'b',
-        '2121':   'c',
-        '211':    'd',
-        '1':      'e',
-        '1121':   'f',
-        '221':    'g',
-        '1111':   'h',
-        '11':     'i',
-        '1222':   'j',
-        '212':    'k',
-        '1211':   'l',
-        '22':     'm',
-        '21':     'n',
-        '222':    'o',
-        '1221':   'p',
-        '2212':   'q',
-        '121':    'r',
-        '111':    's',
-        '2':      't',
-        '112':    'u',
-        '1112':   'v',
-        '122':    'w',
-        '2112':   'x',
-        '2122':   'y',
-        '2211':   'Z',
-        '22222':  '1',
-        '12222':  '2',
-        '11222':  '2',
-        '11122':  '3',
-        '11112':  '4',
-        '11111':  '5',
-        '21111':  '6',
-        '22111':  '7',
-        '22211':  '8',
-        '22221':  '9'
-}
+_exit = 5
 
 
 # Morse Code Class
 class mocoder():
+
+    # Morse codes are decoded with 1's and 2's instead of 0's and 1's to avoid
+    # sending "false" values from the Arduino
+    _morse_codes = {
+            '12':     'a',
+            '2111':   'b',
+            '2121':   'c',
+            '211':    'd',
+            '1':      'e',
+            '1121':   'f',
+            '221':    'g',
+            '1111':   'h',
+            '11':     'i',
+            '1222':   'j',
+            '212':    'k',
+            '1211':   'l',
+            '22':     'm',
+            '21':     'n',
+            '222':    'o',
+            '1221':   'p',
+            '2212':   'q',
+            '121':    'r',
+            '111':    's',
+            '2':      't',
+            '112':    'u',
+            '1112':   'v',
+            '122':    'w',
+            '2112':   'x',
+            '2122':   'y',
+            '2211':   'Z',
+            '22222':  '1',
+            '12222':  '2',
+            '11222':  '2',
+            '11122':  '3',
+            '11112':  '4',
+            '11111':  '5',
+            '21111':  '6',
+            '22111':  '7',
+            '22211':  '8',
+            '22221':  '9'
+    }
 
     # This is where you set up the connection to the serial port.
     def __init__(self, sport=True):
@@ -71,7 +74,7 @@ class mocoder():
         self.current_word = ''
         self.current_symbol = ''
 
-    # This should receive an integer in range 0-3 from the Arduino via a serial
+    # This should receive an integer in range 1-4 from the Arduino via a serial
     # port
     def read_one_signal(self, port=None):
         connection = port if port else self.serial_port
@@ -81,30 +84,43 @@ class mocoder():
             if data:
                 return data
 
+    def update_current_symbol(self, signal):
+        self.current_symbol += signal
+
+    def update_current_word(self, symbol):
+        self.current_word += symbol
+
+    def handle_symbol_end(self):
+        symbol = self._morse_codes.get(self.current_symbol, '')
+        self.update_current_word(symbol)
+        self.current_symbol = ''
+
+    def handle_word_end(self):
+        self.handle_symbol_end()
+        print(self.current_word)
+        self.current_word = ''
+
+    def process_signal(self, sig):
+        if (sig == _dot or sig == _dash):
+            self.update_current_symbol(str(sig))
+        elif (sig == _mpause):
+            self.handle_symbol_end()
+        elif (sig == _lpause):
+            self.handle_word_end()
+        elif (sig == _exit):
+            exit("Done")
+
     # The signal returned by the serial port is one (sometimes 2) bytes, that
     # represent characters of a string.  So, a 2 looks like this: b'2', which
     # is one byte whose integer value is the ascii code 50 (ord('2') = 50).
     # The use of function 'int' on the string converts it automatically.   But,
     # due to latencies, the signal sometimes consists of 2 ascii codes, hence
     # the little for loop to cycle through each byte of the signal.
-
     def decoding_loop(self):
         while True:
             s = self.read_one_signal(self.serial_port)
-            print(s)
-            # for byte in s:
-            #     self.process_signal(int(chr(byte)))
-
-    def process_signal(self, sig):
-        if (sig == _dot or sig == _dash):
-            self.current_symbol += str(sig)
-        if (sig == _mpause):
-            self.current_word += _morse_codes[self.current_symbol]
-            self.current_symbol = ''
-        if (sig == _lpause):
-            self.current_message += self.current_word + ' '
-            self.current_word = ''
-        return True
+            for byte in s:
+                self.process_signal(int(chr(byte)))
 
 
 '''
