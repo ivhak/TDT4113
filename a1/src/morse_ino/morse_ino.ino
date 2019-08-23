53afd0b23bb6d669a9f87a0c4b2037b51e2dd70f
ivhak <hakonseniver@yahoo.no>
2019-08-23 13:32:38 +0200

    Arduino almost working, tweaking needed

--- a1/src/morse_ino/morse_ino.ino
+++ a1/src/morse_ino/morse_ino.ino
@@ -3,77 +3,54 @@
 #define MPAUSE  3
 #define LPAUSE  4
 
-#define T       400 /* ms */
+#define T       300 /* ms */
 
 /*
  * Signal   | Dot | Dash | SPause | MPause | LPause |
- * Duration | T   | 3T   |   T    |   3T   |   7T   |
+ * Duration | <T  | >2T  |  1.5T  |  4.5T  |   7T   |
  */
  
 const int button_pin = 7;
 int button_state;
-int last_state = LOW;
-int last_up;
-int last_down;
-int delta_t;
+int state;
+int start_t;
+float delta_t;
 
 void setup()
 {
-    Serial.begin(9600);
-    pinMode(button_pin, INPUT);
-    last_up = millis();
-    last_down = millis();
+  Serial.begin(9600);
+  pinMode(button_pin, INPUT);
+  state = LOW;
 }
 
 void loop()
 {
-  button_state = digitalRead(button_pin);
-  if (button_state == HIGH && last_state == LOW) {
-    delta_t = millis() - last_down; /* Time spent in LOW state */
-    if (delta_t >= T) {             /* Check if pause is longer than signal pause*/
-      if (delta_t <= 3*T) {
-        Serial.print(MPAUSE);       /* Letter pause */
-      }
-      else if (delta_t <= 7*T) {  
-        Serial.print(LPAUSE);       /* Word pause */
-
-      }
-    }
-    last_state = HIGH;
-    last_up = millis();
-  }
-  if (button_state == LOW && last_state == HIGH) {
-    last_state = LOW;
-    last_down = millis();
-    delta_t = millis() - last_up;
-    if (delta_t <= T) {
-      Serial.print(DOT);
-    }
-    else if (delta_t <= 3*T) {
-      Serial.print(DASH);
-    }
-  }
+  /*
+   * Get the state of the button at the beginning of the loop.
+   * Record how long the button stays in the same state, delta_t, 
+   * then handle what will happen based on the start state
+   */
+  state = digitalRead(button_pin);
+  start_t = millis();
+  while (digitalRead(button_pin) == state)
+    ;
+  delta_t = millis() - start_t;
+  handle(state, delta_t);
 }
 
-void test() {
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(MPAUSE);
-  Serial.print(DOT);
-  Serial.print(MPAUSE);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(LPAUSE);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(MPAUSE);
-  Serial.print(DOT);
-  Serial.print(MPAUSE);
-  Serial.print(DOT);
-  Serial.print(DOT);
-  Serial.print(LPAUSE);
+void handle(int state, int delta_t) {
+  if (state) {
+    if (delta_t >= 2*T) {
+      Serial.print(DASH);
+    }
+    else {
+      Serial.print(DOT);
+    }
+  } else if (delta_t > 1*T && !state) { /* Check if state has been low longer than SPAUSE */
+    if (delta_t <= 4.5*T) {
+      Serial.print(MPAUSE);
+    } else {
+      Serial.print(LPAUSE);
+    }
+  }
 }
