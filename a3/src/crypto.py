b392f2e07cfa1deb1514b0dcbcd9b1d8ddc4c0d0
ivhak <hakonseniver@yahoo.no>
2019-09-11 18:21:36 +0200

    Change alphabet logic, PyLint

--- a3/src/crypto.py
+++ a3/src/crypto.py
@@ -1,147 +1,251 @@
+'''
+Simulation of cryptographic encoding and decoding of strings
+'''
+
+import random
 import crypto_utils as cu
-import sys
+
+FIRST = 65
+ALPHLEN = 26
 
 
 class Cypher():
+    '''
+    Super class for all the different different cyphers
+    '''
     def __init__(self):
-        self.alphabet = list(range(65, 65+25+1))  # ord('A'), ..., ord('̃~')
+        '''
+        Initialize the alphabet to a the list [0, 1, ..., ALPHLEN-1].
+        '''
+        self.alphabet = list(range(ALPHLEN))
 
-    def decode(self, text):
-        pass
+    def decode(self, text, key):
+        '''
+        Decode <text> using <key>
+        '''
 
-    def encode(self, text):
-        pass
+    def encode(self, text, key):
+        '''
+        Encode <text> using <key>
+        '''
 
-    def verify(self, clear_text):
-        return self.decode(self.encode(clear_text)) == clear_text
+    def verify(self, clear_text, key):
+        '''
+        Verify that the encryption and then decryption of <text> using <key>
+        result in the same string.
+        '''
+        return self.decode(self.encode(clear_text, key), key) == clear_text
 
-    def get_alphabet(self):
-        return [chr(x) for x in self.alphabet]
+    def indexes_to_text(self, ord_list):
+        '''
+        Translate a list of numbers to characters. FIRST is the ordinal of the
+        character in the alphabet, i.e. FIRST=65 means the first char is 'A'
+        '''
+        return "".join([chr(FIRST + i) for i in ord_list])
 
 
 class Ceasar(Cypher):
-    def __init__(self):
-        super().__init__()
+    '''
+    This cypher encodes each character by adding a number
+    '''
 
     def decode(self, text, key):
-        s = self.alphabet[0]
-        enc = [s + (ord(c) - s - key) % len(self.alphabet) for c in text]
-        return "".join([chr(n) for n in enc])
+        '''
+        Decoding a ceasar cypher with the key value <key> is the same as
+        encoding using the key -<key>
+        '''
+        return self.encode(text, -key)
 
     def encode(self, text, key):
-        s = self.alphabet[0]
-        enc = [s + (ord(c) - s + key) % len(self.alphabet) for c in text]
-        return "".join([chr(n) for n in enc])
+        '''
+        For each charachter in <text>, find it's index in our alphabet
+        (ord(c) - FIRST), then add <key> and wrap around by using the mod
+        of the length of the alphabet (<ALPHLEN>).
+        '''
+        enc = [(ord(c) - FIRST + key) % ALPHLEN for c in text]
+        return self.indexes_to_text(enc)
 
 
 class Multiplication(Cypher):
-    def __init__(self):
-        super().__init__()
+    '''
+    This cypher encodes each character by multiplying it by a number
+    '''
 
     def decode(self, text, key):
-        s = self.alphabet[0]
-        a_len = len(self.alphabet)
-
-        enc = [s + (ord(c) - s) * cu.modular_inverse(key, a_len) % a_len
+        '''
+        For each char in <text>, find it's index in our alphabet
+        (ord(c) - FIRST), then multiply the modular inverse of the key
+        and wrap around using mod of the length of tha alphabet (<ALPHLEN>).
+        '''
+        dec = [((ord(c) - FIRST) * cu.modular_inverse(key, ALPHLEN)) % ALPHLEN
                for c in text]
-
-        return "".join([chr(n) for n in enc])
+        return self.indexes_to_text(dec)
 
     def encode(self, text, key):
-        s = self.alphabet[0]
-        a_len = len(self.alphabet)
-
-        enc = [s + ((ord(c) - s) * key) % a_len for c in text]
-
-        return "".join([chr(n) for n in enc])
+        '''
+        For each char in <text>, find it's index in our alphabet
+        (ord(c) - FIRST), then multiply by <key> and wrap around using the
+        mod of the length of tha alphabet (<ALPHLEN>).
+        '''
+        enc = [((ord(c) - FIRST) * key) % ALPHLEN for c in text]
+        return self.indexes_to_text(enc)
 
 
 class Affine(Cypher):
+    '''
+    This cypher combines the Multiplication- and Ceasar-cypher.
+    '''
     def __init__(self):
         self.ceasar = Ceasar()
         self.multi = Multiplication()
         super().__init__()
 
     def decode(self, text, key):
+        '''
+        <key> is a tuple (n, m) where n is the key for the
+        Multiplication-cypher, and m is is the key for the Ceasar-cypher.
+        First decode the Ceasar-cypher using n, then decode the result of that
+        with the Multiplication-cypher using m
+        '''
         c_dec = self.ceasar.decode(text, key[1])
         decoded = self.multi.decode(c_dec, key[0])
         return decoded
 
     def encode(self, text, key):
+        '''
+        <key> is a tuple (n, m) where n is the key for the
+        Multiplication-cypher, and m is is the key for the Ceasar-cypher.
+        '''
         multi_enc = self.multi.encode(text, key[0])
         encoded = self.ceasar.encode(multi_enc, key[1])
         return encoded
 
 
 class Unbreakable(Cypher):
-    def __init__(self):
-        super().__init__()
+    '''
+    This cypher encodes a string using another string as the ke
+    '''
 
     def decode(self, text, key):
-        s = self.alphabet[0]
-        a_len = len(self.alphabet)
-
-        # TODO
-        decrypt_key = [chr(s + (a_len - ord(c) % a_len)) for c in key]
-        print(decrypt_key)
+        '''
+        To decode <text> we find the "inverse" of <key>, and encode using this
+        inverted key.
+        '''
+        dec_key = self.indexes_to_text(
+            [(ALPHLEN - (ord(c) - FIRST) % ALPHLEN) % ALPHLEN for c in key])
+        return self.encode(text, dec_key)
 
     def encode(self, text, key):
-        s = self.alphabet[0]
-        a_len = len(self.alphabet)
-        repeated_key = "".join([key[i % len(key)] for i in range(len(text))])
-        print(repeated_key)
+        '''
+        To encode <text> we first repeat the string in <key> enough times to be
+        the same length as <text>. Then we encrypt by adding the value of each
+        char in <text> with the corresponding char in the repeated key.
+        '''
+        rep_key = "".join([key[i % len(key)] for i in range(len(text))])
+        enc = [(ord(c) + ord(k) - 2*FIRST) %
+               ALPHLEN for c, k in zip(text, rep_key)]
+        return self.indexes_to_text(enc)
 
-        return "".join([chr(s + ((ord(c) - s) + (ord(k) - s)) % a_len)
-                        for c, k in zip(text, repeated_key)])
+
+class RSA(Cypher):
+    '''
+    Prime numbers are sick bro!
+    '''
+
+    def decode(self, text, key):
+        pass
+
+    def encode(self, text, key):
+        pass
 
 
 class Person():
+    '''
+    Super class for sending/recieving ciphers.
+    '''
     def __init__(self, cypher):
         self.key = None
         self.cypher = cypher
 
     def set_key(self, key):
+        '''
+        Set the cypher key
+        '''
         self.key = key
 
     def get_key(self):
+        '''
+        Get the cypher key
+        '''
         return self.key
 
-    def operate_cipher(self, text):
-        pass
+    def operate_cypher(self, text):
+        '''
+        Cypher <text>
+        '''
 
 
 class Sender(Person):
-    def __init__(self, cypher):
-        super().__init__(cypher)
+    '''
+    Class for creation of a cypher
+    '''
 
-    def operate_cipher(self, text):
+    def operate_cypher(self, text):
         return self.cypher.encode(text, self.key)
 
 
 class Reciever(Person):
-    def __init__(self, cypher):
-        super().__init__(cypher)
+    '''
+    Class for decyphering
+    '''
 
-    def operate_cipher(self, text):
+    def operate_cypher(self, text):
         return self.cypher.decode(text, self.key)
+
+    def generate_keys(self, bits=12):
+        '''
+        Generate an RSA-key
+        '''
+        p = cu.generate_random_prime(bits)
+        q = cu.generate_random_prime(bits)
+        while p == q:
+            q = cu.generate_random_prime(bits)
+
+        n = p * q
+        phi = (p-1)*(q-1)
+
+        e = random.randint(2, phi)
+        d = cu.modular_inverse(e, phi)
+
+        self.key = (n, d)
+        return (n, e)
 
 
 def main():
-    c = Unbreakable()
 
-    s = Sender(c)
-    s.set_key("PIZZA")
+    c = Ceasar()
+    m = Multiplication()
+    a = Affine()
+    u = Unbreakable()
+    text = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 
-    s = Reciever(c)
-    s.set_key("PIZZA")
+    print("Ceasar:  ", c.verify(text, 2))
+    print("Multi:   ", m.verify(text, 3))
+    print("Affine:  ", a.verify(text, (3, 2)))
+    print("Unbreak: ", u.verify(text, "PIZZA"))
 
     text = "HEMMELIGHET"
-
     print(text)
-    crypt = s.operate_cipher(text)
-    print(crypt)
-    decrypt = s.operate_cipher(crypt)
-    print(decrypt)
+    enc = u.encode(text, "PIZZA")
+    print(enc)
+    dec = u.decode(enc, "PIZZA")
+    print(dec)
+
+    # u = Unbreakable()
+    # text = "HEI"
+    # key = "PIZZA"
+    # print(key)
+    # u.decode(text, key)
 
 
 if __name__ == '__main__':
