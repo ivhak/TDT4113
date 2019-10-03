aa4e12efb9f01d953865035334a24f89ff5f1ed3
ingridlovas <56069161+ingridlovas@users.noreply.github.com>
2019-10-03 13:48:34 +0200

    Update keypad.py

---
+++ a5/src/keypad.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+class keypad: 
+  
+  def __init__(self, rows, columns):
+    self.rows = rows 
+    self.columns = columns
+    
+ def setup(self):
+     GPIO.SETMODE(GPIO.BCM)
+     for rp in self.rows:
+         GPIO.setup(rp, GPIO.OUT)
+     for cp in self.columns:
+         GPIO.setup(cp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+ 
+ #Må legge inn delay her for å hindre støy
+ def do_polling(self):
+     for rp in self.rows:
+         GPIO.output(rp, GPIO.HIGH)
+         for cp in self.columns: 
+             GPIO.input(cp) == GPIO.HIGH
+             return True
+         GPIO.output(rp, GPIO.LOW)
+     return False
+ 
+ def get_next_signal(self):
+     do_polling = self.do_polling()
+     while not do_polling:
+           do_polling = self.do_polling()
+     # Gi beskjed til agent om at knapp er trykket ned
